e20ab0f1edbd4d3cc5ea4d3c460706689b9fbad1
fedor.fominykh <fedor@anna.money>
2022-12-26 12:12:50 +0400

    Day #22 skeleton

---
+++ Day22.cs
@@ -0,0 +1,16 @@
+namespace adventofcode2022;
+
+public class Day22 : PuzzleBase
+{
+    public override void Solve()
+    {
+        var lines = ReadLines();
+        Console.WriteLine(Solve1(lines));
+    }
+
+    private static long Solve1(List<string> lines)
+    {
+        return 0;
+    }
+
+}
